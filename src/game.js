d626214bfacb7b71b5b967f56ded839064a65759
green_gnome <crosfire2011@gmail.com>
2019-07-31 22:09:23 +0300

    add basic moving methods

--- src/game.js
+++ src/game.js
@@ -21,8 +21,8 @@ export default class Game {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
     activePiece = {
         x: 0,
@@ -33,4 +33,60 @@ export default class Game {
             [0, 0, 0]
         ]
     };
+
+    moveLeft() {
+        this.activePiece.x -= 1;
+
+        if(this.hasCollision()) {
+            this.activePiece.x += 1;
+        }
+    }
+
+    moveRight() {
+        this.activePiece.x += 1;
+
+        if(this.hasCollision()) {
+            this.activePiece.x -= 1;
+        }
+    }
+
+    moveDown() {
+        this.activePiece.y += 1;
+
+        if(this.hasCollision()) {
+            this.activePiece.y -= 1;
+            this.lockPiece();
+        }
+    }
+
+    hasCollision() {
+        const { x: activeX, y: activeY, block } = this.activePiece;
+
+        for (let y = 0; y < block.length; y++) {
+            for (let x = 0; x < block[y].length; x++) {
+                if(
+                    block[y][x] && 
+                    ((this.playfield[activeY + y] === undefined || this.playfield[activeY + y][activeX + x] === undefined) ||
+                    this.playfield[activeY + y][activeX + x])
+                    ) {
+                    return true;
+                }            
+            }            
+        }
+        
+        return false;
+    }
+
+    lockPiece() {
+        const { x: activeX, y: activeY, block } = this.activePiece;
+
+        for (let y = 0; y < block.length; y++) {
+            for (let x = 0; x < block[y].length; x++) {
+                if(block[y][x]) {
+                    this.playfield[activeY + y][activeX + x] = block[y][x];
+                }
+            }            
+        }
+    }
+    
 }
